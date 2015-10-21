d1f619c6e75cfe15c1b89872d081db19b70dc2e8
Patrick Burdine <patrick.burdine@gmail.com>
2015-10-20 17:58:22 -0700

    commit first round python game

---
+++ guess-game0.py
@@ -0,0 +1,24 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 23 2015, 02:52:03) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+================================ RESTART ================================
+>>> 
+Welcome!
+Guess the number: 1
+Too low!
+Game over!
+>>> ================================ RESTART ================================
+>>> 
+Welcome!
+Guess the number: 5
+You win!
+Game over!
+>>> ================================ RESTART ================================
+>>> 
+Welcome!
+Guess the number: 7
+Too high!
+Game over!
+>>> 
